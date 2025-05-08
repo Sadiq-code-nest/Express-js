acad50de99569437d38fcd17e46ba667ca7b4b08
Sadiq <sadiqict63@gmail.com>
2025-05-08 16:26:10 +0600

    Http Response Tutorial 5 done

--- httpResponse/app.js
+++ httpResponse/app.js
@@ -1,12 +1,13 @@
 const express_js = require('express');
 const app = express_js();
 
+// Reuired from Routers
 const require_userRouter = require('../expressRouter/Routers/user.router');
 const require_adminRouter = require('../expressRouter/Routers/admin.router');
-const { STATUS_CODES } = require('http');
 
 app.use('/want/api/', require_userRouter);
 app.use('/want/api/', require_adminRouter);
+
 
 // Sending a JSON file
 app.get('/signin', (request, response) => {
@@ -21,18 +22,34 @@ app.get('/signin', (request, response) => {
     response.redirect("/want/api/login");
 });
 
-//Sending html register file
-app.use('/', (request, response) => {
-    response.statusCode = 204;
+//Sending html index file
+app.get('/', (request, response) => {
+    response.statusCode = 200;
     response.sendFile((__dirname + '/views/index.html'));
 });
 
-//Sending html index file
-app.use('/signup', (request, response) => {
-    response.statusCode = 204;
+//Sending html register  file
+app.get('/register', (request, response) => {
+    response.statusCode = 200;
     response.sendFile((__dirname + '/views/register.html'));
 });
 
 
+// Cookie add and delete
+
+app.get('/cookie', (request, response) => {
+  response.cookie("name","sadiq");
+  response.cookie("Age",25);
+
+  // Cookie deleting --- 
+response.clearCookie("name");
+
+//Headers value passing
+response.append("name", "Arif")
+  response.end();
+});
+
+
+// app.get('/', (request, response) => response.send('Its a GET request'));
 app.use((request, response) => response.send('404 not found'));
 module.exports = app;
