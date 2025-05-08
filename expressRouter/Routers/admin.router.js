8e3462caa9d0061ab00d3815b85cbf9c6cc89033
Sadiq <sadiqict63@gmail.com>
2025-05-08 17:23:24 +0600

    05.08.2025

--- expressRouter/Routers/admin.router.js
+++ expressRouter/Routers/admin.router.js
@@ -3,7 +3,6 @@ const Router= express.Router();
 
 Router.get('/about', (request, response) => response.send('Its a GET About request'));
 Router.get('/contact', (request, response) => response.send('Its a GET contact request'));
-Router.get('/register', (request, response) => response.send('Its a GET Register request'));
 Router.get('/login', (request, response) => response.send('Its a GET login request'));
 
 module.exports=Router;
